b2e110953b7ce7b3728158a9532691278063a908
rahul <rahul@hackerrank.com>
2015-06-16 17:07:07 +0530

    Custom Checker supporting MSSIM, PSNR, pixelsimilarity, L2 similarity

--- customchecker.cpp
+++ customchecker.cpp
@@ -14,7 +14,6 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
-//using namespace std;
 using namespace cv;
 using namespace std;
 using namespace Json;
