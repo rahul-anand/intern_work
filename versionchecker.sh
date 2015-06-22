4b82e0a5682c27fb977368690ece62b753bca743
rahul-anand <rahul.anand@research.iiit.ac.in>
2015-06-22 19:25:38 +0530

    Create versionchecker.sh

---
+++ versionchecker.sh
@@ -0,0 +1,153 @@
+#bash
+bash --version
+echo "$BASH_VERSION"
+
+#BF
+
+#C
+gcc -v
+ 
+#CPP
+g++ -v
+
+#Clojure
+#clojure
+#*clojure-version*
+
+#CSHARP
+dmcs --version
+
+#CLISP
+clisp --version
+
+#D
+dmd --version
+
+#Dart
+dart --version
+
+#Text
+cat --version
+
+#ErLang
+erl -eval 'erlang:display(erlang:system_info(otp_release)), halt().'  -noshell
+
+#Fsharp
+fsharpc
+
+#Go
+go version
+
+#Groovy
+groovy -version
+
+#Haskell
+ghc --version
+
+#Javascript
+node -v
+
+#Lua
+lua -v
+
+#MYSQL
+mysql --version
+
+#OBJECTIVEC
+clang --version
+
+#OCAML
+ocaml -version
+
+#OPEN_COBOL
+cobc --version
+
+#    OPEN_JAVA
+gcj --version
+
+
+#ORACLE
+sqlplus -v 
+#RACKET
+racket -v
+
+#TSQL
+
+
+#DB2
+
+
+#PASCAL
+fpc -iW
+
+#PERL
+perl -v
+
+#PHP
+php -v
+
+#PROLOG
+gplc -v
+
+#PYTHON
+python --version
+
+#PYTHON3
+python3 --version
+
+#R
+R --version
+
+#RUBY:
+ruby -v
+
+#SBCL
+sbcl --version
+
+
+#SCALA
+scalac -version
+
+#SCHEME
+csc -version
+
+#SUN_JAVA:
+java -version
+
+#SUN_JAVA8:
+java -version
+
+#VISUAL_BASIC:
+vbnc -help | grep version
+
+#LOLCODE
+lci -v
+
+#SMALLTALK
+gst -v
+
+#TCL
+echo 'puts [info patchlevel];exit 0' | tclsh
+
+#WhiteSpace
+
+#XQuery
+#basex
+
+#MAVEN
+mvn -v
+
+#OCTAVE
+octave -v 
+
+#Android
+ant -version
+
+#Rust
+rustc --version
+
+#Swift
+    
+
+
+   
